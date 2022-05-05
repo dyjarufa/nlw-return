1a5c71f4775a03f3e8e8e9817114856f8e8bf1ef
jady-santos <jady.santos@vodafone.com>
2022-05-05 23:53:49 +0100

    feat: create feedback route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,25 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
+
+app.use(express.json()); // middleware do express para receber json
+
+app.post('/feedbacks', async (req, res) => {
+  // console.log(req.body)
+ 
+  const {type, comment, screenshot } = req.body; // ordem dos parametros precisa seguir a ordem que foram enviado pelo insominia
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+
+  return res.status(201).json({ data: feedback});
+})
 
 app.get('/users', (request, response) => {
   return response.send('Hey there!');
